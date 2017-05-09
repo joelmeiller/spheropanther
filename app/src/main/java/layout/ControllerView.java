8fc03fa463e8a625cfae24cb7e81d7edc505a81c
joelmeiller <joel.meiller@students.fhnw.ch>
2017-05-09 21:55:55 +0200

    improve controller grid

--- app/src/main/java/layout/ControllerView.java
+++ app/src/main/java/layout/ControllerView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 
 import ch.fhnw.edu.emoba.spheropantherapp.R;
@@ -14,6 +15,8 @@ import ch.fhnw.edu.emoba.spheropantherapp.R;
 
 public class ControllerView extends View {
 
+    private static int RADIUS = 150;
+
     private Paint paint;
 
     public ControllerView(Context context) {
@@ -21,13 +24,53 @@ public class ControllerView extends View {
 
         // create the Paint and set its color
         paint = new Paint();
-        paint.setColor(Color.BLUE);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawCircle(0, 0, 300, paint);
+        int xC = (int) canvas.getWidth() / 2;
+        int yC = RADIUS + 20;
+
+        // Draw Lines
+        // *************
+        int color = ContextCompat.getColor(getContext(), R.color.colorLine);
+        paint.setColor(color);
+        paint.setStrokeWidth(4);
+
+        // Horizontal
+        int xStart = xC - RADIUS;
+        int yStart = yC;
+        int xEnd = xC + RADIUS;
+        int yEnd = yC;
+        canvas.drawLine(xStart, yStart, xEnd, yEnd, paint);
+
+        // Vertical
+        xStart = xC;
+        yStart = yC - RADIUS;
+        xEnd = xC;
+        yEnd = yC + RADIUS;
+        canvas.drawLine(xStart, yStart, xEnd, yEnd, paint);
+
+        // Draw Circles
+        // *************
+        color = ContextCompat.getColor(getContext(), R.color.colorCircle);
+        paint.setColor(color);
+
+        // Outer circle
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(6);
+        canvas.drawCircle(xC, yC, RADIUS, paint);
+
+        // First inner circle
+        paint.setStrokeWidth(2);
+        canvas.drawCircle(xC, yC, RADIUS * 2 / 3, paint);
+
+        // Second inner circle
+        paint.setStrokeWidth(2);
+        canvas.drawCircle(xC, yC, RADIUS / 3, paint);
+
+
     }
 }
